6739586e88ac65c81ac775c4934e65d937afd886
Glitch (emojistreamer) <none>
2019-04-28 19:10:24 +0000

    Adapting project for Glitch runtime

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /** [at]healeycodes
  * EmojiStreamer - we consume our express/express-ws app here. See appFactory.js and emoji-streamer.py
  */
+const python = process.env.PY;
 const { spawn } = require('child_process');
-const emojiPiper = spawn('python', ['./emoji-piper.py'])
+const emojiPiper = spawn(python, ['./emoji-piper.py']);
 const app = require('./appFactory')(emojiPiper);
 
 const PORT = process.env.PORT || 8080;
